3ed1b08b32d356a5c19481f875a9f2f751a65ac1
WarLocs <cod.addicte1@gmail.com>
2018-03-10 22:42:31 +0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>System for automatic detection and implementation of YouTube video by Pierre Leocadie</title>
+    <title>System for automatic detection and implementation of YouTube video by WarLocs</title>
 </head>
 <body>
 
